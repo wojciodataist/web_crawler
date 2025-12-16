04a7fe313184bd551c6648cbb48952313df5d714
wojciodataist <wojtowiczmb@gmail.com>
2025-12-16 15:00:14 +0100

    new file with crawler

--- crawl.py
+++ crawl.py
@@ -1,15 +1,21 @@
 from urllib.parse import urlparse
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
+import requests
 
 def normalize_url(input_url):
     url = urlparse(input_url)
     new_url = url.netloc + url.path
-    return new_url
+    new_url = new_url.rstrip("/")
+    return new_url.lower()
 
 def get_h1_from_html(html):
     soup = BeautifulSoup(html, 'html.parser')
-    return soup.h1.get_text()
+    h1 = soup.find("h1")
+    if h1 is not None:
+        return h1.get_text()
+    else:
+        return ""
 
 def get_first_paragraph_from_html(html):
     paragraph_tag = None
@@ -51,3 +57,40 @@ def extract_page_data(html, page_url):
     data["outgoing_links"] = get_urls_from_html(html, page_url)
     data["image_urls"] = get_images_from_html(html, page_url)
     return data
+
+def get_html(url):
+    r = requests.get(url, headers={"User-Agent": "BootCrawler/1.0"})
+    if r.status_code >= 400:
+        r.raise_for_status()
+    content_type = r.headers.get("Content-Type", "")
+    if "text/html" not in content_type:
+        raise ValueError("Response is not HTML")
+    return r.text
+
+def crawl_page(base_url, current_url=None, page_data=None):
+    if current_url is None:
+        current_url = base_url
+    if page_data is None:
+        page_data = {}
+
+    normalized_url = normalize_url(current_url)
+
+    if normalized_url in page_data:
+        return page_data
+
+    print(f"crawling {current_url}")
+
+    try:
+        html = get_html(current_url)
+    except Exception as e:
+        print(e)
+        return page_data
+
+    page_info = extract_page_data(html, current_url)
+    page_data[normalized_url] = page_info
+
+    urls = get_urls_from_html(html, current_url)
+    for url in urls:
+        crawl_page(base_url, url, page_data)
+
+    return page_data
